61a0a9424d8e7b06b0233c49df094120de0295ad
devudilip <devu.dilip@gmail.com>
2014-10-22 01:43:10 +0530

    authors module added

---
+++ db/migrate/20141021200311_create_authors.rb
@@ -0,0 +1,18 @@
+class CreateAuthors < ActiveRecord::Migration
+  def change
+    create_table :authors do |t|
+      t.string :pen_name
+      t.string :birth_place
+      t.string :name
+      t.boolean :sex
+      t.string :spouse
+      t.string :time_period
+      t.integer :poems_found
+      t.text :information
+
+      t.timestamps
+    end
+    add_index :authors, :name
+  end
+end
+
---
+++ spec/factories/authors.rb
@@ -0,0 +1,6 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :author do
+  end
+end
