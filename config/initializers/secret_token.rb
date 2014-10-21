0773bed05cff604ac61ba1ab2b0f481163ea5f98
devudilip <devu.dilip@gmail.com>
2014-10-22 00:40:38 +0530

    Modalane commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LanguageMining::Application.config.secret_key_base = 'd8c8c9491529875544c806999452bccb79cb91823789899ef0c1860db785e58625e6da673e76080efae352a66dfcf7442acf42209f5738ac6a86855e887a6c8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LanguageMining::Application.config.session_store :cookie_store, key: '_language_mining_session'
