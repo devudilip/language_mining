d13ae0fbce7f0658c816088255beebfcd66f15c5
devudilip <devu.dilip@gmail.com>
2014-10-26 00:56:43 +0530

    localization setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_local_language
+
+  def set_local_language
+    I18n.locale = 'kn'
+  end
+
 end
