eeb8ca6021d8bac708d43a2db836954de0007387
devudilip <devu.dilip@gmail.com>
2014-10-25 23:50:08 +0530

    added vachana theame deleted unwanted files.

--- app/models/poem.rb
+++ app/models/poem.rb
@@ -1,2 +1,4 @@
 class Poem < ActiveRecord::Base
+  belongs_to :author
+  belongs_to :book
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ LanguageMining::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'homes#index'
+  resources :poems
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
