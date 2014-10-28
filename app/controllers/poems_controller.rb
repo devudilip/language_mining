f8b3389ef3093d64e79c1ad95e5740dcbc11b3ca
devudilip <devu.dilip@gmail.com>
2014-10-29 01:39:49 +0530

    language updated , author search

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,7 @@
 class PoemsController < ApplicationController
 
   def index
+    @author = Author.find_by_id(params[:author])
     @poems = Poem.paginate(:page => params[:page], :per_page => 14)
   end
 
