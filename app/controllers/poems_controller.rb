e5c1849104d74f5a7e4989694693d73f579cee63
devudilip <devu.dilip@gmail.com>
2015-06-16 00:54:18 +0530

    Concordance link ajax load

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -4,6 +4,10 @@ class PoemsController < ApplicationController
     params[:start_letter] = params[:start_letter] ? params[:start_letter] : "ಅ"
     @poems = Poem.includes(:author).start_letter(params[:start_letter]).paginate(:page => params[:page], :per_page => 15)
     @poem_concordances = Poem.concordance_list
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
 end
