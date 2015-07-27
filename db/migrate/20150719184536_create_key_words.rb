5621042a01d8b910a52fafb9feb54a63d19371da
devudilip <devu.dilip@gmail.com>
2015-07-27 23:40:55 +0530

    text field in to long text limit

--- db/migrate/20150719184536_create_key_words.rb
+++ db/migrate/20150719184536_create_key_words.rb
@@ -3,8 +3,8 @@ class CreateKeyWords < ActiveRecord::Migration
     create_table :key_words do |t|
       t.string :word
       t.integer :count
-      t.text :poem_ids
-      t.text :author_ids
+      t.text :poem_ids, :limit => 4294967295
+      t.text :author_ids, :limit => 4294967295
       t.timestamps
     end
     add_index :key_words, :word
